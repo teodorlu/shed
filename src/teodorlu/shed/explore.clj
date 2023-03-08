ef837a98179710a867fb88c12d0af63f04b2e473
Teodor Heggelund <git@teod.eu>
2023-03-08 17:14:10 +0100

    Move exploration off into `teodorlu.shed.explore`

---
+++ src/teodorlu/shed/explore.clj
@@ -0,0 +1,58 @@
+(ns teodorlu.shed.explore)
+
+;; not for reuse! Simply change, and evaluate.
+;; Please don't depend on this file.
+
+(comment
+  ;; 2023-03-08
+  ;; destructuring question on Slack
+  ;; https://clojurians.slack.com/archives/C053AK3F9/p1678280301686469
+
+  (let [x {:type "test" :meta {:rate 0.1}}
+        {type :type} x
+        {{rate :rate} :meta} x
+        ]
+    [type rate])
+  ;; => ["test" 0.1]
+
+  (let [{type :type {:keys [rate]} :meta}
+        {:type "test" :meta {:rate 0.1}}]
+    [type rate])
+  ;; => ["test" 0.1]
+
+  (let [{:keys [type] {:keys [rate]} :meta}
+        {:type "test" :meta {:rate 0.1}}]
+    [type rate])
+  ;; => ["test" 0.1]
+
+  (let [m {:type "test" :meta {:rate 0.1}}
+        f (fn [{:keys [type] {:keys [rate]} :meta}] [type rate])]
+    (f m))
+  ;; => ["test" 0.1]
+
+
+  ;; destructuring:
+  (let [{:keys [type] {:keys [rate]} :meta}
+        {:type "test" :meta {:rate 0.1}}]
+    [type rate])
+
+  ;; straightforward:
+  (let [data {:type "test" :meta {:rate 0.1}}]
+    [(:type data) (:rate (:meta data))])
+
+  )
+
+(comment
+  ;; 2023-03-08
+  ;; https://clojurians.slack.com/archives/C053AK3F9/p1678288277210489
+
+  [:app :data :people 12346]
+  [:app :data :things 123456]
+  (let [some-data {:app {:data {:people {12346 {:name "Ryan"}}
+                                :things {123456 {:make "Toyota" :color "Blue"}}}}}
+        people (fn [data id] (get-in data [:app :data :people id]))
+        things (fn [data id] (get-in data [:app :data :things id]))]
+    [(people some-data 12346)
+     (things some-data 123456)])
+  ;; => [{:name "Ryan"} {:make "Toyota", :color "Blue"}]
+  )
--- src/teodorlu/shed/update_repos.clj
+++ src/teodorlu/shed/update_repos.clj
@@ -41,59 +41,5 @@
   ;; => 245
   )
 
-(comment
-  ;; destructuring question on Slack
-  ;; https://clojurians.slack.com/archives/C053AK3F9/p1678280301686469
-
-  (let [x {:type "test" :meta {:rate 0.1}}
-        {type :type} x
-        {{rate :rate} :meta} x
-        ]
-    [type rate])
-  ;; => ["test" 0.1]
-
-  (let [{type :type {:keys [rate]} :meta}
-        {:type "test" :meta {:rate 0.1}}]
-    [type rate])
-  ;; => ["test" 0.1]
-
-  (let [{:keys [type] {:keys [rate]} :meta}
-        {:type "test" :meta {:rate 0.1}}]
-    [type rate])
-  ;; => ["test" 0.1]
-
-  (let [m {:type "test" :meta {:rate 0.1}}
-        f (fn [{:keys [type] {:keys [rate]} :meta}] [type rate])]
-    (f m))
-  ;; => ["test" 0.1]
-
-
-  ;; destructuring:
-  (let [{:keys [type] {:keys [rate]} :meta}
-        {:type "test" :meta {:rate 0.1}}]
-    [type rate])
-
-  ;; straightforward:
-  (let [data {:type "test" :meta {:rate 0.1}}]
-    [(:type data) (:rate (:meta data))])
-
-  )
-
-(comment
-
-  ;; https://clojurians.slack.com/archives/C053AK3F9/p1678288277210489
-
-  [:app :data :people 12346]
-  [:app :data :things 123456]
-  (let [some-data {:app {:data {:people {12346 {:name "Ryan"}}
-                                :things {123456 {:make "Toyota" :color "Blue"}}}}}
-        people (fn [data id] (get-in data [:app :data :people id]))
-        things (fn [data id] (get-in data [:app :data :things id]))]
-    [(people some-data 12346)
-     (things some-data 123456)])
-  ;; => [{:name "Ryan"} {:make "Toyota", :color "Blue"}]
-  )
-
-
 (defn -main [& _args]
   (do-update-repos! (fs/cwd)))
