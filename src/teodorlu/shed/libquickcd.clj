b1d3730a92c511b8866c9ff19520ec4fdfb3ff57
Teodor Heggelund <git@teod.eu>
2023-03-10 23:21:14 +0100

    Try to adapt babashka.process/exec to take :dir
    ... and fail!
    I'm getting some very weird behavior.

--- src/teodorlu/shed/libquickcd.clj
+++ src/teodorlu/shed/libquickcd.clj
@@ -1,9 +1,10 @@
 (ns teodorlu.shed.libquickcd
   (:require
    [babashka.fs :as fs]
-   [babashka.process :refer [shell]]
-   [clojure.string :as str]
-   [clojure.core :exclude [next]]))
+   [babashka.process :as process :refer [shell]]
+   [clojure.core :exclude [next]]
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 ;; aimed to be called from a ,cd function
 ;;
@@ -16,16 +17,63 @@
     (when (= 0 (:exit fzf-result))
       (str/trim (:out fzf-result)))))
 
-(def ^:dynamic *current-directory* (fs/cwd))
+(def -program-resolver (requiring-resolve 'babashka.process/-program-resolver))
+(def default-escape (requiring-resolve 'babashka.process/-program-resolver))
+(defmacro ^:no-doc
+  if-has-exec [then else]
+  (if (requiring-resolve 'babashka.process/has-exec?)
+    then else))
+
+(defn exec2
+  "Copied from babashka.process/exec -- but modified to support :dir"
+
+  {:arglists '([opts? & args])}
+  [& args]
+  (let [{:keys [cmd opts]} (process/parse-args args)]
+    (let [{:keys [escape env extra-env]
+           :or {escape default-escape}
+           :as opts} opts
+          cmd (if (and (string? cmd)
+                       (not (.exists (io/file cmd))))
+                (process/tokenize cmd)
+                cmd)
+          str-fn (comp escape str)
+          _ (prn {:mapv mapv :str-fn str-fn :cmd cmd})
+          cmd (mapv str-fn cmd)
+          _ (prn "confused.")
+          arg0 (or (:arg0 opts)
+                   (first cmd))
+          cmd (let [program-resolver (:program-resolver opts -program-resolver)
+                    [program & args] cmd]
+                (into [(program-resolver program)] args))
+          [program & args] cmd
+          args (cons arg0 args)
+          ^java.util.Map env (into (or env (into {} (System/getenv))) extra-env)]
+      (if-has-exec
+       (org.graalvm.nativeimage.ProcessProperties/exec (or (:dir opts) (fs/path program))
+                                                       (into-array String args)
+                                                       env)
+       (throw (ex-info "exec is not supported in non-GraalVM environments" {:cmd cmd}))))))
 
 (defn walk-loop
   [{:keys [start next]}]
   (loop [loc start]
     (if-let [next-loc (next loc)]
       (recur next-loc)
-      (babashka.process/exec {} (or (System/getenv "SHELL") "bash")))))
+      (do
+        (prn loc)
+        (prn (fs/absolutize loc))
+        (read-line)
+        (exec2 #_ {:dir "/tmp" #_ (fs/absolutize loc)}
+               (or (System/getenv "SHELL") "bash"))))))
 
 (defn -main [& args]
   (walk-loop {:start (or (first args) ".")
               :next  (fn [loc] (fzf (sort (map str (fs/list-dir loc)))))})
   ,)
+
+
+(comment
+
+
+  )
