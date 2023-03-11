841aac4344ccbe41783538800be0929231494697
Teodor Heggelund <git@teod.eu>
2023-03-11 14:43:18 +0100

    Woah, it actually works

--- src/teodorlu/shed/libquickcd.clj
+++ src/teodorlu/shed/libquickcd.clj
@@ -3,12 +3,24 @@
    [babashka.fs :as fs]
    [babashka.process :as process :refer [shell]]
    [clojure.core :exclude [next]]
-   [clojure.java.io :as io]
    [clojure.string :as str]))
 
 ;; aimed to be called from a ,cd function
 ;;
-;; for installation instructions, see:
+;; libquickcd must be installed with a shell wrapper for tight shell
+;; integration. The binary name (libquickcd) must match the name you installed
+;; the babashka script with.
+;;
+;;   ,cd() {
+;;     exec libquickcd "$@"
+;;     # or
+;;     #
+;;     #   exec ,libquickcd "$@"
+;;     #
+;;     # if you installed with commas
+;;   }
+;;
+;; for more details, see:
 ;;
 ;;   https://github.com/teodorlu/shed/tree/master/contrib/quickcd/
 
@@ -17,63 +29,20 @@
     (when (= 0 (:exit fzf-result))
       (str/trim (:out fzf-result)))))
 
-(def -program-resolver (requiring-resolve 'babashka.process/-program-resolver))
-(def default-escape (requiring-resolve 'babashka.process/-program-resolver))
-(defmacro ^:no-doc
-  if-has-exec [then else]
-  (if (requiring-resolve 'babashka.process/has-exec?)
-    then else))
-
-(defn exec2
-  "Copied from babashka.process/exec -- but modified to support :dir"
-
-  {:arglists '([opts? & args])}
-  [& args]
-  (let [{:keys [cmd opts]} (process/parse-args args)]
-    (let [{:keys [escape env extra-env]
-           :or {escape default-escape}
-           :as opts} opts
-          cmd (if (and (string? cmd)
-                       (not (.exists (io/file cmd))))
-                (process/tokenize cmd)
-                cmd)
-          str-fn (comp escape str)
-          _ (prn {:mapv mapv :str-fn str-fn :cmd cmd})
-          cmd (mapv str-fn cmd)
-          _ (prn "confused.")
-          arg0 (or (:arg0 opts)
-                   (first cmd))
-          cmd (let [program-resolver (:program-resolver opts -program-resolver)
-                    [program & args] cmd]
-                (into [(program-resolver program)] args))
-          [program & args] cmd
-          args (cons arg0 args)
-          ^java.util.Map env (into (or env (into {} (System/getenv))) extra-env)]
-      (if-has-exec
-       (org.graalvm.nativeimage.ProcessProperties/exec (or (:dir opts) (fs/path program))
-                                                       (into-array String args)
-                                                       env)
-       (throw (ex-info "exec is not supported in non-GraalVM environments" {:cmd cmd}))))))
+(defn shell-init-dir-command [shell loc]
+  ;; Note: does not yet handle spaces in loc, I need some escaping.
+  (cond (= (fs/file-name shell) "zsh") ["zsh" "-c" (str "cd " (fs/absolutize loc) "; exec zsh")]
+        (= (fs/file-name shell) "bash") ["bash" "-c" (str "cd " (fs/absolutize loc) "; exec bash")]
+        :else ["bash" "-c" (str "cd " (fs/absolutize loc) "; exec bash")]))
 
 (defn walk-loop
   [{:keys [start next]}]
   (loop [loc start]
     (if-let [next-loc (next loc)]
       (recur next-loc)
-      (do
-        (prn loc)
-        (prn (fs/absolutize loc))
-        (read-line)
-        (exec2 #_ {:dir "/tmp" #_ (fs/absolutize loc)}
-               (or (System/getenv "SHELL") "bash"))))))
+      (apply process/exec (shell-init-dir-command (System/getenv "SHELL") (fs/absolutize loc))))))
 
 (defn -main [& args]
   (walk-loop {:start (or (first args) ".")
               :next  (fn [loc] (fzf (sort (map str (fs/list-dir loc)))))})
   ,)
-
-
-(comment
-
-
-  )
