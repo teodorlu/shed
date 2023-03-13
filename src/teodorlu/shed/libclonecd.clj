ea462311bcf00fbba61ce0a646e9e6c41c8ef050
Teodor Heggelund <git@teod.eu>
2023-03-13 15:07:52 +0100

    Finish up pure clojure ,clonecd impl

--- src/teodorlu/shed/libclonecd.clj
+++ src/teodorlu/shed/libclonecd.clj
@@ -1,4 +1,8 @@
-(ns teodorlu.shed.libclonecd)
+(ns teodorlu.shed.libclonecd
+  (:require
+   [clojure.string :as str]
+   [babashka.fs :as fs]
+   [babashka.process :refer [shell]]))
 
 ;; shell alias: ,clonecd
 ;;
@@ -11,6 +15,43 @@
 ;;      $ libclonecd teodorlu/shed
 ;;      !SHELLEVAL cd /home/teodorlu/dev/teodorlu/shed
 
-(defn -main [& _argv]
-  (println "libclonecd")
-  (println "!SHELLEVAL cd /home/teodorlu/dev/teodorlu/shed"))
+;; ignore parameterization while prototyping.
+;;
+;; git repos are stored in ~/dev/ORGANIZATION/PROJECT
+;;
+;; a REPOSPEC is ORG/PROJECT as a string, or {:org ORG :project PROJECT} as data
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn repospec->path [repospec]
+  (let [{:keys [org project]} repospec]
+    (str (fs/expand-home "~/dev") "/" org "/" project)))
+
+(defn repospec->git-clone-arg [repospec]
+  (let [{:keys [org project]} repospec]
+    (str "git@github.com:" org "/" project ".git")))
+
+(defn run [repospec]
+  (let [path (repospec->path repospec)]
+    (if (fs/exists? path)
+      (println "!SHELLEVAL cd" path)
+      (do
+        (shell ["git" "clone" (repospec->git-clone-arg repospec)])
+        (println "!SHELLEVAL cd" path)))))
+
+(defn parse-opts [argv]
+  (let [[org project] (str/split (first argv) #"/")]
+    {:org org :project project}))
+
+(defn valid-args? [argv]
+  (and argv
+       (first argv)
+       (= 2 (count (str/split (first argv) #"/")))))
+
+(defn -main [& argv]
+  (when (not (valid-args? argv))
+    (println "invalid arguments:" (pr-str argv))
+    (println "Usage: libclonecd ORG/REPO")
+    (System/exit 1))
+  (let [opts (parse-opts argv)]
+    (run opts)))
