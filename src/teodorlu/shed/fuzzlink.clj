56cfd8c83308a9efd901640d9344b26234cdc4af
Teodor Heggelund <git@teod.eu>
2023-05-19 13:33:12 +0200

    Cleanup HTML

--- src/teodorlu/shed/fuzzlink.clj
+++ src/teodorlu/shed/fuzzlink.clj
@@ -1,7 +1,9 @@
 (ns teodorlu.shed.fuzzlink
   (:require
    [clojure.data.xml]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [hickory.core :as htmlparse]
+   [clojure.walk :refer [prewalk]]))
 
 (def clojure-deref-rss-uri "https://clojure.org/feed.xml")
 (def clojure-deref-text (slurp clojure-deref-rss-uri))
@@ -17,6 +19,18 @@
        (filter #(str/includes? % "Clojure Deref"))
        (map str/trim)
        (filter #(str/starts-with? %  "<div"))))
+
+(->>
+ (map htmlparse/as-hiccup
+      (htmlparse/parse-fragment (first deref-bodies)))
+ (prewalk (fn [x]
+            (if (vector? x)
+              (into [] (remove #{} x))
+              x)))
+ (filter (fn [section]
+           (and
+            (= :div (first section)))))
+ )
 
 (defn -main [& args]
   (prn (count deref-bodies)))
