3008a7ae602786e854a433b7e9f737af274079c9
Teodor Heggelund <git@teod.eu>
2024-06-02 15:30:17 +0200

    begin setting up for weeknotes

---
+++ src/teodorlu/shed/libweeknote.clj
@@ -0,0 +1,4 @@
+(ns teodorlu.shed.libweeknote)
+
+;; Did you read https://doingweeknotes.com/, and want to write weeknotes?
+;; Libweeknotes is a bag of utilities that can help ease your pain.
---
+++ test/teodorlu/shed/libweeknote_test.clj
@@ -0,0 +1,7 @@
+(ns teodorlu.shed.libweeknote-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [teodorlu.shed.libweeknote :as libweeknote]))
+
+(deftest teodorlu.shed.libweeknote-test
+  (is (= 1 1)))
